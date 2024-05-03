7c946d28e3e6d4b1c72d4b6a3ffb8f7141defbf5
Mustaaaaa <moustafaibrahim1512@gmail.com>
2024-05-03 18:36:31 +0200

    add: esercizio completato

--- index.php
+++ index.php
@@ -8,14 +8,32 @@
 <body>
     <form action="./server.php" method="POST">
         <p>
-            <label for="paragraph">Paragrafo</label>
-            <input type="text" name="paragraph" placeholder="Scrivi qui il yuo messaggio...">
+            <label for="paragraph">Paragrafo: </label>
+            <input type="text" name="paragraph" class="paragraph-input" placeholder="Scrivi qui il yuo messaggio...">
         </p>
         <p>
-            <label for="word">Parola</label>
-            <input type="text" name="word" placeholder="Scrivi qui la parola da censurare...">
+            <label for="word">Parola: </label>
+            <input type="text" name="word" class="word-input" placeholder="Scrivi qui la parola da censurare...">
         </p>
         <button>Invia</button>
     </form>
 </body>
 </html>
+<style>
+    .paragraph-input { 
+        border: 2px solid red;
+        border-radius: 10px;
+        padding: 10px;
+        overflow-y: scroll;
+    }
+    .word-input { 
+        margin-top: 10px;
+        border: 2px solid green;
+        border-radius: 10px;
+        padding: 10px;
+    }
+    label{
+        font-size: 25px;
+        font-weight: 700;
+    }
+</style>
--- server.php
+++ server.php
@@ -1,11 +1,12 @@
 <?php
-var_dump($_POST);
+// var_dump($_POST);
 // var_dump($_GET);
 
 $paragraph = $_POST['paragraph'];
 $word = $_POST['word'];
 $paragraph_lenght = strlen($paragraph);
-
+$paragraph_with_censored_word = str_replace($word, '***', $paragraph, $censored_word_count);
+$paragraph_with_censored_word_lenght = strlen($paragraph_with_censored_word);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,8 +17,30 @@ $paragraph_lenght = strlen($paragraph);
 </head>
 <body>
     <div>
-        <h3>Paragrafo: <?php echo $paragraph ?></h3>
-        <p>Lunghezza del paragrafo originale: <?php echo $paragraph_lenght ?></p>
+        <div class="first-paragraph">
+            <h2>Paragrafo:</h2>
+            <h3><?php echo $paragraph ?></h3>
+            <p>Lunghezza del paragrafo originale: <?php echo $paragraph_lenght ?></p>
+        </div>
+        <div class="second-paragraph">
+            <h2>Paragrafo con la parola scritta censurata:</h2>
+            <h3><?php echo $paragraph_with_censored_word ?></h3>
+            <p>Lunghezza del paragrafo originale: <?php echo $paragraph_with_censored_word_lenght ?></p>
+            <p>Totale parole censurate: <?php echo $censored_word_count ?></p>
+        </div>
     </div>
 </body>
 </html>
+<style>
+    .first-paragraph { 
+        border: 2px solid red;
+        border-radius: 10px;
+        padding: 10px;
+    }
+    .second-paragraph { 
+        margin-top: 10px;
+        border: 2px solid green;
+        border-radius: 10px;
+        padding: 10px;
+    }
+</style>
